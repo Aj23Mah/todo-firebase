13b532a7f15d2900ce81d103f8550ea7c4dccbbb
ajayMaharjan <ajaymaharjan758@gmail.com>
2022-09-06 21:34:00 +0545

    firebase

---
+++ src/Firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBAMMS34mq2ueJvlA83SklaV7Fr8hc2kk0",
+    authDomain: "ajaya-todo-app.firebaseapp.com",
+    databaseURL: "https://ajaya-todo-app-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "ajaya-todo-app",
+    storageBucket: "ajaya-todo-app.appspot.com",
+    messagingSenderId: "183970219566",
+    appId: "1:183970219566:web:2d408b0412801f7b6641bf",
+    measurementId: "G-7QJ6F71J0P"
+  };
+  
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
